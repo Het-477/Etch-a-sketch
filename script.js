ad79504f79ee2c1ffc67bf713ed3353744eacdbf
Het Gurjar <130950604+Het-477@users.noreply.github.com>
2025-02-01 13:12:59 +0530

    idk why git is telling me its modified but it is so i committed it

---
+++ script.js
@@ -0,0 +1,25 @@
+// const PAD_SIDE = 600;
+
+function generateSketchpadPixels(size = 16) { // default sketchpad size
+    const sketchpad = document.getElementById("sketchpad");
+    sketchpad.innerHTML = ""; // resets (clears) the pad when its resized
+    sketchpad.style.setProperty('--grid-size', size);
+    // sketchpad.style.setProperty('--pad-side', PAD_SIDE);
+
+    for (let i = 0; i < size * size; i++) {
+        const pixel = document.createElement("div");
+        pixel.classList.add("sketchpad-pixel");
+        pixel.addEventListener("mouseover", () => {
+            pixel.style.backgroundColor = "black";
+        });
+        sketchpad.appendChild(pixel);
+    }
+}
+
+function eraseSketchPad() {
+    document.querySelectorAll(".sketchpad-pixel").forEach(pixel => {
+        pixel.style.backgroundColor = "white";
+    });
+}
+
+generateSketchpadPixels()
